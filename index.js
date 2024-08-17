7104b21bd05b825aba0d1d825f5a144629d89796
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-08-17 10:11:47 +0600

    all done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 9000
 const app = express()
 
 const corsOptions = {
-    origin: ['http://localhost:5173','http://localhost:5174'],
+    origin: ['http://localhost:5173','http://localhost:5174','https://scic-server-kappa.vercel.app'],
     Credential: true,
     optionSuccessStatus: 200,
 }
